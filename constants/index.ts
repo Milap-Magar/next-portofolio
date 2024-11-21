bc5bb0ebca413e7fd7d222cb126d11fc4cdcbd1c
milap-magar <milapmagar12@gmail.com>
2024-11-21 14:13:41 +0545

    Added: More navigation features

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,6 @@
 export const navItems = [
-  { id: "#main", title: "src/Page/Main.tsx", key: 1 },
-  { id: "#projects", title: "src/Page/Projects.tsx", key: 2 },
-  { id: "#about", title: "src/Page/About.tsx", key: 3 },
-  { id: "#contact", title: "src/Page/Contact.tsx", key: 4 },
+  { id: "#main", title: "src/Pages/Main.tsx", key: 1 },
+  { id: "#projects", title: "src/Pages/Projects.tsx", key: 2 },
+  { id: "#about", title: "src/Pages/About.tsx", key: 3 },
+  { id: "#contact", title: "src/Pages/Contact.tsx", key: 4 },
 ];

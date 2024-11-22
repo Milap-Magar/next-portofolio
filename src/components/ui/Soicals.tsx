f5216ccd4688c370c5041fdcdfe11a1bc667d04c
milap-magar <milapmagar12@gmail.com>
2024-11-22 14:02:48 +0545

    Changed: Icons color changed

--- src/components/ui/Soicals.tsx
+++ src/components/ui/Soicals.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { socials } from "../../../constants";
 import Image from "next/image";
+import Link from "next/link";
 
 const Soicals = () => {
   return (
     <ul className="w-[250px] md:flex gap-6 hidden">
-      {socials.map((item) => (
-        <li key={item.id} className="cursor-pointer">
-          <Image
-            src={item.icons}
-            alt="icons"
-            width={24}
-            height={24}
-            className="w-[35px] "
-          />
+      {socials.map((social) => (
+        <li key={social.id} className="cursor-pointer">
+          <Link href={social.link} target="_blank">
+            <Image
+              src={social.icons}
+              alt="icons"
+              width={24}
+              height={24}
+              className="w-[35px] "
+            />
+          </Link>
         </li>
       ))}
     </ul>
